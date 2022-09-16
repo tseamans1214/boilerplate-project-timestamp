5ee62bca29d154db733eef9b1b511097697ed7c9
Tommy Seamans <tlseamans1214@gmail.com>
2022-09-15 18:23:34 -0700

    Check for invalid dates

--- index.js
+++ index.js
@@ -25,6 +25,10 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/:date", function (req, res) {
+  var testDate = new Date(req.params.date);
+  if (testDate.getTime() !== testDate.getTime()) {
+    res.json({ error : "Invalid Date" });
+  }
   if(req.params.date.includes("-")) {
     var date = new Date(req.params.date);
     const timestampInMs = date.getTime();
